64e174ad6bf7cc1f988988bb8c486249e21e4a36
EpicMice <micecd@gmail.com>
2021-09-14 08:37:48 -0500

    .

---
+++ Game/Lib/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿namespace TBRPG_1.Game.Lib
+{
+    public class Rectangle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
